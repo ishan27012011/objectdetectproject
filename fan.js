ce01513c1ee13b3f6cf1575c665d64e346d472e8
ishan27012011 <81416295+ishan27012011@users.noreply.github.com>
2022-10-14 21:52:55 +0530

    V3 of this project.

---
+++ fan.js
@@ -0,0 +1,47 @@
+img = "";
+status = "";
+objects = [];
+
+function setup() {
+    canvas = createCanvas(640, 420,);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("but").innerHTML = "Status: Detecting Object";
+}
+
+function preload() {
+    img = loadImage("fan.png");
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    objectDetector.detect(img, gotResults);
+    status = true;
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        for (let i = 0; i < objects.length; i++) {
+            document.getElementById("but").innerHTML = "Status: Objects Detected";
+            fill("red")
+            percent = floor(objects[i].confidence * 100);
+            textSize(20);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            strokeWeight(3);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function air() {
+    window.location.href = "microwave.html";
+}
+function tv() {
+    window.location.href = "tv.html";
+}
+function light() {
+    window.location.href = "couch.html";
+}
+function fan() {
+    window.location.href = "fan.html";
+}
+function backTo() {
+    window.location.href = "index.html";
+}
